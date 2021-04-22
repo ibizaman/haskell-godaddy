1a387cd2ab434101ec920e554e74052828d7b570
ibzaman <ibizapeanut@gmail.com>
2021-04-22 01:14:20 -0700

    update dependencies versions

--- release.nix
+++ release.nix
@@ -1,21 +1,25 @@
 { compiler ? "ghc865"
-, pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/f0d8828b86c8105f722e9b1cceec50bcba1c9df6.tar.gz") {}
+, pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/20.09.tar.gz") {}
 }:
 
 let
   inherit (pkgs.haskell.lib) dontCheck;
 
+  commonOverrides = self: super: {
+      Cabal = dontCheck (self.callHackage "Cabal" "3.2.0.0" {});
+      hpack = dontCheck (self.callHackage "hpack" "0.34.2" {});
+  };
+
   haskellPackages = pkgs.haskell.packages.${compiler}.override {
     overrides = self: super: rec {
       haskell-godaddy = self.callCabal2nix "haskell-godaddy" ./. { };
-      Cabal = dontCheck (self.callHackage "Cabal" "3.2.1.0" {});
-    };
+      hspec-wai = dontCheck (self.callHackage "hspec-wai" "0.9.0" {});
+    } // (commonOverrides self super);
   };
 
   shellHaskellPackages = pkgs.haskell.packages.${compiler}.override {
     overrides = self: super: rec {
-      Cabal = dontCheck (self.callHackage "Cabal" "3.2.1.0" {});
-    };
+    } // (commonOverrides self super);
   };
 
   project = haskellPackages.haskell-godaddy;
